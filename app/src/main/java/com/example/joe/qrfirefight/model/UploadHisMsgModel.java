9063b74f3e35af2828672938bf81cc1698c25e80
18145288 <2275501257@qq.com>
2019-10-17 15:30:09 +0800

    222

---
+++ app/src/main/java/com/example/joe/qrfirefight/model/UploadHisMsgModel.java
@@ -0,0 +1,32 @@
+package com.example.joe.qrfirefight.model;
+
+/**
+ * Created by 18145288 on 27/5/2019.
+ */
+
+public class UploadHisMsgModel {
+
+    /**
+     * Code : 1
+     * Msg : 成功
+     */
+
+    private int Code;
+    private String Msg;
+
+    public int getCode() {
+        return Code;
+    }
+
+    public void setCode(int Code) {
+        this.Code = Code;
+    }
+
+    public String getMsg() {
+        return Msg;
+    }
+
+    public void setMsg(String Msg) {
+        this.Msg = Msg;
+    }
+}
